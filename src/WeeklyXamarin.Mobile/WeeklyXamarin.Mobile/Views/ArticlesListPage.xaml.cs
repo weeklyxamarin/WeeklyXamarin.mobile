57ff76c49563bfa74979f6cf6e294dfb1f76f610
Kym Phillpotts <kphillpotts@gmail.com>
2020-10-15 00:38:57 +1100

    Added a empty bookmark view with a lottie monkey

--- src/WeeklyXamarin.Mobile/WeeklyXamarin.Mobile/Views/ArticlesListPage.xaml.cs
+++ src/WeeklyXamarin.Mobile/WeeklyXamarin.Mobile/Views/ArticlesListPage.xaml.cs
@@ -11,6 +11,9 @@ using WeeklyXamarin.Core.ViewModels;
 using WeeklyXamarin.Core.Models;
 using Container = WeeklyXamarin.Core.Services.Container;
 using Microsoft.Extensions.DependencyInjection;
+using System.IO;
+using System.Reflection;
+using Lottie.Forms;
 
 namespace WeeklyXamarin.Mobile.Views
 {
@@ -40,7 +43,50 @@ namespace WeeklyXamarin.Mobile.Views
             ViewModel.EditionId = EditionId;
 
             await ViewModel.LoadArticlesCommand.ExecuteAsync(false);
-            
+
+            //// read the json
+            //var assembly = Assembly.GetExecutingAssembly();
+            //var resourceName = "WeeklyXamarin.Mobile.bookmarkanimation.json";
+
+            //string json;
+
+            //using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+            //using (StreamReader reader = new StreamReader(stream))
+            //{
+            //    json = reader.ReadToEnd();
+            //}
+
+            ////animationView.SetAnimationFromJson(json);
+
+            //animationView.AnimationSource = AnimationSource.Json;
+            //animationView.Animation = json;
+            //animationView.PlayAnimation();
+
+        }
+
+        //public void SetAnimationFromEmbeddedResource(string resourceName, Assembly assembly = null)
+        //{
+        //    var animationSource = AnimationSource.EmbeddedResource;
+        //    if (assembly == null) 
+        //        assembly = Xamarin.Forms.Application.Current.GetType().Assembly; 
+        //    Animation = $"resource://{resourceName}?assembly={Uri.EscapeUriString(assembly.FullName)}";
+        //}
+
+        private void animationView_OnFailure(object sender, Exception e)
+        {
+            System.Diagnostics.Debug.WriteLine(e.StackTrace);
+        }
+
+        private void animationView_OnAnimationLoaded(object sender, object e)
+        {
+            System.Diagnostics.Debug.WriteLine("Animation Loaded");
+
+        }
+
+        private void animationView_OnPlayAnimation(object sender, EventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine("Playing Animation");
+
         }
     }
 }
