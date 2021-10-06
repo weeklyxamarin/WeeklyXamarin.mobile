4918c1382408a8957117f373629300dbe08a50ea
Lachlan Gordon <lachlanwgordon@gmail.com>
2021-10-06 22:58:10 +1100

    Cleaned up some dead code

--- src/WeeklyXamarin.Mobile/WeeklyXamarin.Blazor/Client/Pages/EditionsPage.razor.cs
+++ src/WeeklyXamarin.Mobile/WeeklyXamarin.Blazor/Client/Pages/EditionsPage.razor.cs
@@ -9,17 +9,17 @@ namespace WeeklyXamarin.Blazor.Client.Pages
 {
     public partial class EditionsPage
     {
-        [Inject] public EditionsViewModel? ViewModel {get;set;}
+        [Inject] public EditionsViewModel? ViewModel { get; set; }
 
         protected override async Task OnAfterRenderAsync(bool firstRender)
         {
             await base.OnAfterRenderAsync(firstRender);
             if (firstRender)
             {
-				if (ViewModel is not null)
-				{
-					await ViewModel.ExecuteLoadEditionsCommand();
-				}
+                if (ViewModel is not null)
+                {
+                    await ViewModel.ExecuteLoadEditionsCommand();
+                }
                 StateHasChanged();
             }
         }
