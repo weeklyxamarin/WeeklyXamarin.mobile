328cfd4f22b6260c34c285c9b522693c4064a37b
smabuk <2011834+smabuk@users.noreply.github.com>
2021-10-07 20:21:35 +0100

    Display version with build date/time

--- src/WeeklyXamarin.Mobile/WeeklyXamarin.Blazor/Server/Program.cs
+++ src/WeeklyXamarin.Mobile/WeeklyXamarin.Blazor/Server/Program.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace WeeklyXamarin.Blazor.Server
@@ -22,5 +23,12 @@ namespace WeeklyXamarin.Blazor.Server
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+
+        public static Version Version { get; set; } = typeof(Program).Assembly
+                        .GetName().Version ?? new();
+        public static string ProductVersion { get; set; } = typeof(Program).Assembly
+                                .GetCustomAttribute<AssemblyInformationalVersionAttribute>()?
+                                .InformationalVersion ?? "";
+
     }
 }
